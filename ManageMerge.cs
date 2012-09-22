3ed0f2b2dcf65b4e6d9f21dcab8e66f0fcedb7b1
Rajeshwaran S P <tuxrajesh@gmail.com>
2012-09-22 00:14:11 -0400

    Adding Files For the Utility

---
+++ ManageMerge.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Reflection;
+using System.IO;
+using System.Collections.Generic;
+
+namespace MergeConfig
+{
+    class ManageMerge
+    {
+        /// <summary>
+        /// Region of Deployment
+        /// </summary>
+        private string region;
+        private bool verbose;
+
+        /// <summary>
+        /// Manage Merge
+        /// </summary>
+        /// <param name="region">Region</param>
+        public ManageMerge(string region, bool verbose)
+        {
+            this.region = region;
+            this.verbose = verbose;
+        }
+
+        /// <summary>
+        /// Perform Merge
+        /// </summary>
+        public List<string> Merge()
+        {
+            List<string> errorMessages = new List<string>();
+
+            // Load Configuration file for this Application
+            FileInfo fileInfo = new FileInfo(Assembly.GetExecutingAssembly().Location);
+            XDocument mergeConfig = XDocument.Load(fileInfo.Directory.FullName + "\\MergeConfig.xml");
+
+            // Select File List to Update
+            var files = mergeConfig.XPathSelectElements("//deploymentRegion[@name='" + region + "']/fileInfo[@fileName]");
+
+            if (files == null)
+                throw new Exception(string.Format("Region Name: {0} specified is not valid.", region));
+
+            foreach (var file in files)
+            {
+                try
+                {
+                    // Process File
+                    XDocument configFile = XDocument.Load(file.Attribute("fileName").Value);
+
+                    if (verbose)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Processing File: {0}", file.Attribute("fileName").Value);
+                    }
+
+                    var manageConfigs = file.Elements();
+                    foreach (var manageConfig in manageConfigs)
+                    {
+                        // Locate element to update
+                        XElement element = configFile.XPathSelectElement(manageConfig.Attribute("findXPath").Value);
+
+                        if (element == null)
+                        {
+                            errorMessages.Add(string.Format("In File: {0}, Path: {1} was not found.", file.Attribute("fileName").Value, manageConfig.Attribute("findXPath").Value));
+                            continue;
+                        }
+
+                        foreach (XAttribute attribute in element.Attributes())
+                        {
+                            // Find Attribute to update
+                            if (attribute.Name == manageConfig.Attribute("replaceAttribute").Value)
+                            {
+                                // Make the update
+                                attribute.SetValue(manageConfig.Attribute("replaceWith").Value);
+                                
+                                if(verbose)
+                                    Console.WriteLine("\t Updated attribute: {0} with {1}", attribute.Name, attribute.Value);
+                            }
+                        }
+                    }
+
+                    // Save the changes to the file
+                    configFile.Save(file.Attribute("fileName").Value);
+
+                    if (verbose)
+                        Console.WriteLine();
+                }
+                catch (Exception ex)
+                {
+                    errorMessages.Add(string.Format("Error Occured For File: {0}. Error: {1}", file.Attribute("fileName").Value, ex.Message));
+                    continue;
+                }
+            }
+
+            return errorMessages;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MergeConfig
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {            
+            try
+            {
+                if (args == null || args.Length < 1)
+                {
+                    DisplayHelp();
+                    return 0;
+                }
+
+                string regionName = string.Empty;
+                bool verbose = false;
+
+                foreach (string arg in args)
+                {
+                    if (arg.Contains("/h") || arg.Contains("/help"))
+                    {
+                        DisplayHelp();
+                        return 0;
+                    }
+
+                    if (arg.Contains("/r:") || arg.Contains("/region:"))
+                    {
+                        regionName = arg.Split(':')[1];
+                        continue;
+                    }
+
+                    if (arg.Contains("/v") || args.Contains("/verbose"))
+                        verbose = true;
+                }
+
+                if (string.IsNullOrEmpty(regionName))
+                    throw new Exception("regionname was not supplied");
+
+                ManageMerge manager = new ManageMerge(regionName, verbose);
+                List<string> errorMessages = manager.Merge();
+
+                if (errorMessages.Count > 0)
+                {
+                    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+                    foreach (string errorMessage in errorMessages)
+                        Console.WriteLine(errorMessage);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Total {0} errors.", errorMessages.Count);
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+                Console.WriteLine("Error occured: {0}",ex.Message);
+                Console.WriteLine("Process Exited.");
+                return -1;
+            }
+
+            Console.WriteLine("Process Complete.");
+            return 0;
+        }
+
+        static void DisplayHelp()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Syntax:");
+            Console.WriteLine("\t MergeConfig.exe {/r | /region}:<region_name> {/v | /verbose}");
+            Console.WriteLine();
+            Console.WriteLine("Examples:");
+            Console.WriteLine("\t MergeConfig.exe /r:Development /v");
+            Console.WriteLine("\t MergeConfig.exe /r:Development /verbose");
+            Console.WriteLine("\t MergeConfig.exe /region:Development");
+            Console.WriteLine();
+        }
+    }
+}
